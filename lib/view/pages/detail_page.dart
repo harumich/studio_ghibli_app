253e7c69065509d20b526ce18479ce284a3888cc
harumich <talutalu.haru1486@gmail.com>
2022-08-20 01:09:09 +0900

    Step: 3-2 映画詳細画面のUIを整える

--- lib/view/pages/detail_page.dart
+++ lib/view/pages/detail_page.dart
@@ -8,20 +8,42 @@ class MovieDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop(context),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListTile(
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.white,),
+                  onPressed: () => Navigator.pop(context),
+                ),
               ),
-            ),
-            Image.network(
-              "https://www.ghibli.jp/gallery/laputa047.jpg",
-            ),
-          ],
+              const Spacer(),
+              Text('天空の城 ラピュタ',style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30)),
+              const SizedBox(height: 30),
+              Image.network(
+                "https://www.ghibli.jp/gallery/laputa047.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 20),
+                child: Container(
+                  child: const Text('2000年/宮崎駿'),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              Text(
+                  'ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐ナスカ大佐',
+                  style: TextStyle(color: Colors.white)
+              ),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
